c2151c2fdcca517350befcf1c843d930c455ae4c
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2022-05-07 18:54:50 +0200

    wip

--- terraform/services.tf
+++ terraform/services.tf
@@ -5,14 +5,23 @@ module "zelos_system_service_external_secrets" {
 }
 
 module "zelos_system_service_cert_manager" {
-  source            = "/Users/jakoberpf/Code/jakoberpf/terraform/modules/kubernetes/certmanager-deployment" # "jakoberpf/certmanager-deployment/kubernetes"
+  depends_on = [
+    module.zelos_system_service_external_secrets
+  ]
 
-  deploy_manager    = true
-  deploy_reflector  = true
+  source            = "jakoberpf/certmanager-deployment/kubernetes"
+
+  # compartment = "athena-installer"
+  deploy_manager    = true # TODO remove and make default
+  deploy_reflector  = true # TODO remove and make default
   cloudflare_tokens = var.cloudflare_tokens
 }
 
 module "zelos_system_service_longhorn" {
+  depends_on = [
+    module.zelos_system_service_cert_manager
+  ]
+
   source = "/Users/jakoberpf/Code/jakoberpf/terraform/modules/kubernetes/longhorn-deployment" # "jakoberpf/longhorn-deployment/kubernetes"
 
   compartment                = "athena-installer"
@@ -25,10 +34,14 @@ module "zelos_system_service_longhorn" {
 }
 
 module "zelos_system_service_monitoring" {
+  depends_on = [
+    module.zelos_system_service_cert_manager
+  ]
+
   source = "/Users/jakoberpf/Code/jakoberpf/terraform/modules/kubernetes/monitoring-deployment" # "jakoberpf/monitoring-deployment/kubernetes"
 
   compartment           = "athena-installer"
-  ingress_dns           = var.grafana_ingress_dns
+  ingress_dns           = var.grafana_ingress_dns # TODO rename to grafana_ingress_dns
   grafana_adminPassword = var.grafana_adminPassword
   grafana_root_url      = var.grafana_root_url
   grafana_client_id     = var.grafana_client_id
@@ -36,6 +49,15 @@ module "zelos_system_service_monitoring" {
   grafana_auth_url      = var.grafana_auth_url
   grafana_token_url     = var.grafana_token_url
   grafana_api_url       = var.grafana_api_url
+
+  # prometheus_ingress_dns   = var.grafana_ingress_dns
+  # prometheus_adminPassword = var.grafana_adminPassword
+  # prometheus_root_url      = var.grafana_root_url
+  # prometheus_client_id     = var.grafana_client_id
+  # prometheus_client_secret = var.grafana_client_secret
+  # prometheus_auth_url      = var.grafana_auth_url
+  # prometheus_token_url     = var.grafana_token_url
+  # prometheus_api_url       = var.grafana_api_url
 }
 
 # module "zelos_system_service_flux" {
